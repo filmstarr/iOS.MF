f05d15e980bfed99bb368d1e4d2926a929ccacc9
Ross Huelin <filmstarr@hotmail.com>
2014-05-21 09:45:10 +0000

    Settings.english-utf8.php edited online with Bitbucket

--- iOS7_theme_v1/languages/Settings.english-utf8.php
+++ iOS7_theme_v1/languages/Settings.english-utf8.php
@@ -5,6 +5,6 @@ global $settings;
 
 // Important! Before editing these language files please read the text at the top of index.english.php.
 $txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.png';
-$txt['theme_description'] = 'An iOS 7 styled iPhone theme<br /><br />iOS7 by <a href="mailto:filmstarr@hotmail.com.com">filmstarr</a>.';
+$txt['theme_description'] = 'An iOS 7 styled iPhone theme<br /><br />iOS7 by <a href="mailto:filmstarr@hotmail.com.com">filmstarr</a><br /><br />Based on <a url="http://custom.simplemachines.org/themes/index.php?lemma=2089">SMF4iPhone</a> by Fabius, butchs and FarFromPerfection.';
 
 ?>
