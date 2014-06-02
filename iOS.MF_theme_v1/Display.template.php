f358032882de35041fa1e82b20002d19e9a320fe
Ross Huelin <filmstarr@hotmail.com>
2014-06-02 22:58:21 +0100

    Reorder functions.

--- iOS.MF_theme_v1/Display.template.php
+++ iOS.MF_theme_v1/Display.template.php
@@ -218,48 +218,6 @@ echo '</div>
 
   template_control_paging();
 }
-
-function template_control_paging()
-{
-  global $context, $txt;
-
-  echo '
-
-  <script>
-
-    $(function(){
-
-      Hammer($("#previousPage")).on("tap", function(event) {
-        window.location.href=\'', $context['links']['prev'] ,'\';
-      });
-
-      Hammer($("#nextPage")).on("tap", function(event) {
-        window.location.href=\'', $context['links']['next'] ,'\';
-      });
-
-      Hammer($("#previousPage")).on("hold", function(event) {
-        window.location.href=\'', $context['links']['first'] ,'\';
-      });
-
-      Hammer($("#nextPage")).on("hold", function(event) {
-        window.location.href=\'', $context['links']['last'] ,'\';
-      });
-
-    });
-
-  </script>
-
-
-  <div class="page buttons">
-  
-  <button id="previousPage" class="button" ', $context['page_info']['current_page']==1 ? 'disabled="disabled"' : '', '>', $txt['iPrev'], '</button>
-  
-  <button id="pagecount">', $txt['iPage'], ' ', $context['page_info']['current_page'] ,' ', $txt['iOf'] ,' ', ($context['page_info']['num_pages']==0) ? '1' : $context['page_info']['num_pages'] ,'</button>
-  
-  <button id="nextPage" class="button" ', ($context['page_info']['current_page']==$context['page_info']['num_pages']||$context['page_info']['num_pages']==0) ? 'disabled="disabled"' : '', '>', $txt['iNext'], '</button>
-  
-  </div>';
-}
   
 function quick_reply()
 {    
@@ -355,6 +313,48 @@ echo '</div>';
     </form>';
     
     echo '</div>';
+}
+
+function template_control_paging()
+{
+  global $context, $txt;
+
+  echo '
+
+  <script>
+
+    $(function(){
+
+      Hammer($("#previousPage")).on("tap", function(event) {
+        window.location.href=\'', $context['links']['prev'] ,'\';
+      });
+
+      Hammer($("#nextPage")).on("tap", function(event) {
+        window.location.href=\'', $context['links']['next'] ,'\';
+      });
+
+      Hammer($("#previousPage")).on("hold", function(event) {
+        window.location.href=\'', $context['links']['first'] ,'\';
+      });
+
+      Hammer($("#nextPage")).on("hold", function(event) {
+        window.location.href=\'', $context['links']['last'] ,'\';
+      });
+
+    });
+
+  </script>
+
+
+  <div class="page buttons">
+  
+  <button id="previousPage" class="button" ', $context['page_info']['current_page']==1 ? 'disabled="disabled"' : '', '>', $txt['iPrev'], '</button>
+  
+  <button id="pagecount">', $txt['iPage'], ' ', $context['page_info']['current_page'] ,' ', $txt['iOf'] ,' ', ($context['page_info']['num_pages']==0) ? '1' : $context['page_info']['num_pages'] ,'</button>
+  
+  <button id="nextPage" class="button" ', ($context['page_info']['current_page']==$context['page_info']['num_pages']||$context['page_info']['num_pages']==0) ? 'disabled="disabled"' : '', '>', $txt['iNext'], '</button>
+  
+  </div>';
 }
 
 ?>
