f3f480834b7f7a917d16bf42c7052fe9026eb3b5
Ross Huelin <filmstarr@hotmail.com>
2014-06-04 21:21:38 +0100

    Style login template

--- iOS.MF_theme_v1/Login.template.php
+++ iOS.MF_theme_v1/Login.template.php
@@ -6,55 +6,47 @@ function template_login()
 	global $context, $scripturl, $settings, $txt;
 
 	if (!empty($context['login_errors']))
-		foreach ($context['login_errors'] as $error)
-			echo '<h2>', $error, '</h2>';
+		echo '<div id="loginErrors"><div style="margin-top: 6px;">*', implode('</div><div style="margin-top: 6px;">*', $context['login_errors']), '</div></div>';
 	if (isset($context['description']))
-		echo '<h2>', $context['description'], '</h2>';
+		echo '<div id="loginErrors"><div style="margin-top: 6px;">*', $context['description'], '</div></div>';
 
-echo'<h2></h2>';		
-	echo'<ul class="login" style="clear:both;">
-	
-	<form action="', $scripturl, '?action=login2" name="frmLogin" id="frmLogin" method="post" accept-charset="', $context['character_set'], '" ', empty($context['disable_login_hashing']) ? ' onsubmit="hashLoginPassword(this, \'' . $context['session_id'] . '\');"' : '', '>
-		
-		<li>
-			<div class="field">
-				<div class="fieldname">'. $txt['username'] .'</div>
-				<div class="fieldinfo"><input type="text" name="user" /></div>
-			</div>
-		</li>
-		
-		<li>
-			<div class="field">
-				<div class="fieldname">'. $txt['password'] .'</div>
-				<div class="fieldinfo"><input type="password" name="passwrd" /></div>
-			</div>
-		</li>
-		
-		<li>
-			<div class="last field">
-				<div class="fieldname toggle">'. $txt['iRemember'] .'</div>
-				<div class="fieldinfo toggle">
-					<input type="checkbox" checked="checked" name="cookieneverexp" value="1" id="cookieneverexp">
-				</div>
-			</div>
-		</li>
-		
-	<input type="hidden" name="hash_passwrd" value="" />
-		
-	</ul>
-	
-	<div class="child buttons" style="margin-top:10px !important;">
-	
-	<button type="submit">'. $txt['iSubmit'] .'</button>
-	
-	</div>
 
-';
+
+
+  echo '<div id="quickLogin" style="display: block;">
+  <form action="', $scripturl, '?action=login2" name="frmLogin" id="frmLogin" method="post" accept-charset="', $context['character_set'], '" ', empty($context['disable_login_hashing']) ? ' onsubmit="hashLoginPassword(this, \'' . $context['session_id'] . '\');"' : '', '>
+
+<div class="noLeftPadding inputContainer padTop">';
+  echo'<span class="inputLabel">'. $txt['username'] .'</span>';
+  echo'<input id="user" type="text" tabindex="', $context['tabindex']++, '" name="user" />
+</div>
+<div class="noLeftPadding inputContainer padTop">';
+  echo'<span class="inputLabel">'. $txt['password'] .'</span>';
+  echo'<input type="password" tabindex="', $context['tabindex']++, '" name="passwrd" />
+</div>
+<div class="noLeftPadding inputContainer padTop">';
+  echo'<span class="inputLabel">'. $txt['iRemember'] .'</span>';
+  echo'<input type="checkbox" checked="checked" name="cookieneverexp" value="1" id="cookieneverexp">
+</div>
+    
+  <input type="hidden" name="hash_passwrd" value="" />
+  <div class="buttons" style="margin-top: -9px; padding-bottom: 5px; margin-bottom: 6px;">
+    <button class="button twobuttons" type="submit">' . $txt['login'] . '</button>
+    <button class="button twobuttons" type="button" onclick="go(\'register\')">'. $txt['register'] .'</button>
+  </div>
+  </form>
+  </div>';
+
+
+
+
+
+
 }
 
 function template_kick_guest(){
 global $txt;
-echo '<h2>', empty($context['kick_message']) ? $txt['only_members_can_access'] : $context['kick_message'],'</h2>';
+echo '<div id="loginErrors"><div style="margin-top: 6px;">*', empty($context['kick_message']) ? $txt['only_members_can_access'] : $context['kick_message'],'</div></div>';
 template_login();
 }
 
