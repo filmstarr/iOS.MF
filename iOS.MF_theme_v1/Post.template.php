84fbc290be827377c6b3f0708945327f60561a54
Ross Huelin <filmstarr@hotmail.com>
2014-06-26 22:38:19 +0100

    Quick commit which will hopefully sort issue with not disabling text area.

--- iOS.MF_theme_v1/Post.template.php
+++ iOS.MF_theme_v1/Post.template.php
@@ -12,7 +12,7 @@ function template_main()
         $(".classic").last().hide();
 
         //Deal with the race condition between iOS keyboard showing and the focus event firing
-        var jqElement = $("#', $context['post_box_name'], '");
+        var jqElement = $(".editor").last();
         jqElement.attr("disabled", true);
 
         Hammer(jqElement.on("tap", function(event) {
@@ -108,7 +108,7 @@ echo '</div>';
   
   echo '<div class="child buttons">
   
-  <button class="button" type="submit" onclick="$(\'#', $context['post_box_name'], '\').blur(); $(\'#', $context['post_box_name'], '\').removeAttr(\'disabled\'); $(\'.ui-loader\').last().show();">', $txt['iPost'] ,'</button>
+  <button class="button" type="submit" onclick="$(".editor").last().blur(); $(".editor").last().removeAttr(\'disabled\'); $(\'.ui-loader\').last().show();">', $txt['iPost'] ,'</button>
 
   </div>';
 
