94c6d02bba4b8f81bd12af646db924f710c08753
Ross Huelin <filmstarr@hotmail.com>
2014-07-02 02:16:42 +0100

    Added index to PM

--- iOS.MF_theme_v1/PersonalMessage.template.php
+++ iOS.MF_theme_v1/PersonalMessage.template.php
@@ -14,69 +14,130 @@ function template_folder()
 {
   global $context, $settings, $options, $scripturl, $modSettings, $txt;
 
-  if (empty($settings['show_user_images']) || !empty($options['show_no_avatars']))
+  $messageCount = 0;
+
+  if ($context['display_mode'] != 0 && strpos($_SERVER['REQUEST_URI'], 'pmsg') == false)
   {
-    echo '<style>
-      .message { min-height: initial !important; }
-      #avatar { display: none; }
-      .message_time { margin-bottom: 5px !important; }
-    </style>';
-  }
+    echo '<div class="child buttons noLeftPadding">
+      <button class="button" style="width: 150px;" onclick="$.mobile.changePage(\'' , $scripturl , '?action=pm;sa=send\');">Send Message</button>
+    </div>';
 
-  echo '<script>  
-    $(function() {
-      function handler(event) {
-        event.stopPropagation();
-      }
-      $("a").click(handler);
-    });
-  </script>';
-
-  require_once ($settings[theme_dir].'/ThemeFunctions.php');
-  NavigateToMessageScript();
-
-  echo '<div class="child buttons noLeftPadding">
-    <button class="button" style="width: 150px;" onclick="$.mobile.changePage(\'' , $scripturl , '?action=pm;sa=send\');">Compose Message</button>
-  </div>';
-
-  echo '<ul class="content2">';
-
-    while ($message = $context['get_pmessage']('message'))
+    while ($message = $context['get_pmessage']('subject'))
     {
-      echo'
-        <li>
-          <a id="msg', $message['id'], '"></a>';
-          // Comment out subject stuff
-          //echo '<div class="postDetails">' , $message['counter'] + 1 , '. ', $message['subject'] ,'</div>';
-          echo '
-          <div>
-            <button class="button slimbutton" id="editdel" onclick="$.mobile.changePage(\'', $scripturl, '?action=pm;sa=send;f=', $context['folder'], $context['current_label_id'] != -1 ? ';l=' . $context['current_label_id'] : '', ';pmsg=', $message['id'], ';u=', $message['member']['id'], '\');" >', $txt['reply'] ,'</button>
-            <button class="button slimbutton" id="editdel" onclick="if (confirm(\'', $txt['remove_message'], '?\')) { $.mobile.changePage(\'', $scripturl, '?action=pm;sa=pmactions;pm_actions[', $message['id'], ']=delete;f=', $context['folder'], ';start=', $context['start'], $context['current_label_id'] != -1 ? ';l=' . $context['current_label_id'] : '', ';', $context['session_var'], '=', $context['session_id'], '\'); }"> ', $txt['remove'],' </button>
-          </div>
+      if ($messageCount == 0)
+      {
+        echo '<ul class="content2">';
+      }
 
-          <div class="posterinfo" onclick="$(this).parent().addClass(\'clicked\'); $.mobile.changePage(\'', isset($message['member']['href']) ? $message['member']['href'] : '' ,'\')"><span class="name">', $message['member']['name'] ,'</span>';
-          if (!empty($settings['show_user_images']) && empty($options['show_no_avatars']))
-            if (empty($message['member']['avatar']['image'])) {
-              echo '<div id="avatar" style="background: url('.$settings['theme_url'].'/images/noavatar.png) #F5F5F5 center no-repeat;"></div>';
-            }
-            else {
-              echo '<div id="avatar" style="background: url('.str_replace(' ','%20', $message['member']['avatar']['href']).') #fff center no-repeat;"></div>';
-            }
-          echo '
-        
-          </div>
-          
-
-          <div class="message" onclick="$(this).parent().addClass(\'clicked\'); $.mobile.changePage(\'', $scripturl, '?action=pm;sa=send;f=', $context['folder'], $context['current_label_id'] != -1 ? ';l=' . $context['current_label_id'] : '', ';pmsg=', $message['id'], ';quote', $context['folder'] == 'sent' ? '' : ';u=' . $message['member']['id'], '\');">
-            <span class="message_time" style="font-style: italic;font-size:11px;display:inline-block;margin-bottom:3px;">', str_replace('strong','span',$message['time']) ,'</span><br />
-          ', str_replace(rtrim($scripturl,'/index.php') . '/Smileys/default/', $settings['theme_url'] . '/images/SkypeEmoticons/',str_replace('<strong>Today</strong>','Today',short1($message['body']))) ,'
-          </div>
-
-        </li>';
+      echo '
+      <li onclick="this.className = \'clicked\'; $.mobile.changePage(\''. $scripturl . '?action=pm;pmsg=' . $message['id'] . ';msg' . $message['id'] . '#msg' . $message['id'] .'\');">';
+        echo '<div class="title', ($message['is_unread'] ? ' shortTitle' : '') ,'">', ($context['display_mode'] == 2 ? preg_replace('/\bRe: /', '', $message['subject']) : $message['subject']) ,'</div>';
+        if ($message['is_unread']) {
+          echo '<div class="new">'. $txt['new_button'] .'</div>';
+        }
+        echo '<div class="description">', ($context['from_or_to'] == 'from' ? $message['member']['name'] : (empty($message['recipients']['to']) ? '' : implode(', ', $message['recipients']['to']))) , ', ', $message['time']=='N/A' ? $txt['no'] . ' ' . $txt['topics'] : iPhoneTime($message['timestamp']) , '</div>
+      </li>';
+      $messageCount++;
     }
 
-  echo '</ul>';
-    
+    if ($messageCount != 0)
+    {
+      echo '</ul>';
+    }
+  }
+  else
+  {
+    if ($context['display_mode'] == 0)
+    {
+      echo '<div class="child buttons noLeftPadding">
+        <button class="button" style="width: 150px;" onclick="$.mobile.changePage(\'' , $scripturl , '?action=pm;sa=send\');">Send Message</button>
+      </div>';
+    }
+
+    $subject = "";
+    if (empty($settings['show_user_images']) || !empty($options['show_no_avatars']))
+    {
+      echo '<style>
+        .message { min-height: initial !important; }
+        #avatar { display: none; }
+        .message_time { margin-bottom: 5px !important; }
+      </style>';
+    }
+
+    echo '<script>  
+      $(function() {
+        function handler(event) {
+          event.stopPropagation();
+        }
+        $("a").click(handler);
+      });
+    </script>';
+
+    require_once ($settings[theme_dir].'/ThemeFunctions.php');
+    NavigateToMessageScript();
+
+
+      while ($message = $context['get_pmessage']('message'))
+      {
+        if ($messageCount == 0)
+        {
+          echo '<ul class="content2 ' , ($context['display_mode'] != 0 ? ' firstContent' : '')  , '">';
+        }
+        echo'
+          <li>
+            <a id="msg', $message['id'], '"></a>';
+            echo '
+            <div>
+              <button class="button slimbutton" id="editdel" onclick="$.mobile.changePage(\'', $scripturl, '?action=pm;sa=send;f=', $context['folder'], $context['current_label_id'] != -1 ? ';l=' . $context['current_label_id'] : '', ';pmsg=', $message['id'], ';u=', $message['member']['id'], '\');" >', $txt['reply'] ,'</button>
+              <button class="button slimbutton" id="editdel" onclick="if (confirm(\'', $txt['remove_message'], '?\')) { $.mobile.changePage(\'', $scripturl, '?action=pm;sa=pmactions;pm_actions[', $message['id'], ']=delete;f=', $context['folder'], ';start=', $context['start'], $context['current_label_id'] != -1 ? ';l=' . $context['current_label_id'] : '', ';', $context['session_var'], '=', $context['session_id'], '\'); }"> ', $txt['remove'],' </button>
+            </div>
+
+            <div class="posterinfo" onclick="$(this).parent().addClass(\'clicked\'); $.mobile.changePage(\'', isset($message['member']['href']) ? $message['member']['href'] : '' ,'\')"><span class="name">', $message['member']['name'] ,'</span>';
+            if (!empty($settings['show_user_images']) && empty($options['show_no_avatars']))
+              if (empty($message['member']['avatar']['image'])) {
+                echo '<div id="avatar" style="background: url('.$settings['theme_url'].'/images/noavatar.png) #F5F5F5 center no-repeat;"></div>';
+              }
+              else {
+                echo '<div id="avatar" style="background: url('.str_replace(' ','%20', $message['member']['avatar']['href']).') #fff center no-repeat;"></div>';
+              }
+            echo '
+          
+            </div>
+            
+
+            <div class="message" onclick="$(this).parent().addClass(\'clicked\'); $.mobile.changePage(\'', $scripturl, '?action=pm;sa=send;f=', $context['folder'], $context['current_label_id'] != -1 ? ';l=' . $context['current_label_id'] : '', ';pmsg=', $message['id'], ';quote', $context['folder'] == 'sent' ? '' : ';u=' . $message['member']['id'], '\');">
+              <span class="message_time" style="font-style: italic;font-size:11px;display:inline-block;margin-bottom:3px;">', str_replace('strong','span',$message['time']) ,'</span><br />
+            ', str_replace(rtrim($scripturl,'/index.php') . '/Smileys/default/', $settings['theme_url'] . '/images/SkypeEmoticons/',str_replace('<strong>Today</strong>','Today',short1($message['body']))) ,'
+            </div>
+
+          </li>';
+
+          $subject = $message['subject'];
+          $messageCount++;
+      }
+
+    if ($messageCount != 0)
+    {
+      echo '</ul>';
+    }
+
+    if (strpos($_SERVER['REQUEST_URI'], 'pmsg') == true)
+    {
+      echo '
+      <script>
+        $(function(){
+          $(".theTitle").last().html("', ($context['display_mode'] == 2 ? preg_replace('/\bRe: /', '', $subject) : $subject) ,'");
+        });
+      </script>';
+    }
+  }
+  
+  if ($messageCount==0)
+  echo '
+      <div id="unreadlink" style="padding-top: 0 !important;">
+        ', $txt['msg_alert_none'] , '
+      </div>';
+
   require_once ($settings[theme_dir].'/ThemeControls.php');
   template_control_paging();
 }
@@ -130,11 +191,10 @@ function template_send()
     echo '<style> #newTopic { padding-top: 9px; } </style>';        
   }
 
-  // Comment out subject stuff and hide subject
-  // echo '<div id="newTopic" class="inputContainer">';
-  //   echo '<span class="inputLabel">Topic</span>';
-     echo '<input type="hidden" tabindex="', $context['tabindex']++, '" name="subject" value="' , (empty($context['subject']) || $context['subject'] == $txt['no_subject'] ? 'Sent from iOS.MF' : $context['subject']) , '" maxlength="50" />';
-  // echo '</div>';
+  echo '<div id="newTopic" class="inputContainer">';
+    echo '<span class="inputLabel">Subject</span>';
+    echo '<input type="text" tabindex="', $context['tabindex']++, '" name="subject" value="' , (empty($context['subject']) || $context['subject'] == $txt['no_subject'] ? 'Sent from iOS.MF' : $context['subject']) , '" maxlength="50" />';
+  echo '</div>';
 
   echo '<div id="newTopic" class="inputContainer">';
     echo '<span class="inputLabel">'. $txt['iTo'] .'</span>';
